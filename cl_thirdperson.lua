4503e47c3dd470c0bd47076542eb8cd7d139f43f
Dark Fated <49955245+darkfated@users.noreply.github.com>
2025-01-04 05:45:59 +0300

    Add support Mantle library

--- cl_thirdperson.lua
+++ cl_thirdperson.lua
@@ -33,69 +33,93 @@ local color_background = Color(44, 44, 44)
 local color_close = Color(253, 116, 92)
 
 concommand.Add('third_person_menu', function()
-    local menu_thirdperson = vgui.Create('DFrame')
-    menu_thirdperson:SetSize(300, 175)
-    menu_thirdperson:Center()
-    menu_thirdperson:MakePopup()
-    menu_thirdperson:ShowCloseButton(false)
-    menu_thirdperson:SetTitle('Third Person Settings')
-    menu_thirdperson.Paint = function(_, w, h)
-        draw.RoundedBox(8, 0, 0, w, h, color_background)
-        draw.RoundedBoxEx(8, 0, 0, w, 24, color_header, true, true, false, false)
-    end
+    local menuThirdperson = vgui.Create(Mantle and 'MantleFrame' or 'DFrame')
+    menuThirdperson:SetSize(300, Mantle and 224 or 175)
+    menuThirdperson:Center()
+    menuThirdperson:MakePopup()
+    menuThirdperson:SetTitle('Third Person Settings')
+    menuThirdperson:SetKeyBoardInputEnabled(false)
 
-    local cls = vgui.Create('DButton', menu_thirdperson)
-    cls:SetSize(16, 16)
-    cls:SetPos(menu_thirdperson:GetWide() - cls:GetWide() - 4, 4)
-    cls:SetText('')
-    cls.DoClick = function()
-        menu_thirdperson:Close()
-    end
-    cls.Paint = function(_, w, h)
-        draw.RoundedBox(6, 0, 0, w, h, color_close)
-    end
+    local checkbox = vgui.Create(Mantle and 'MantleCheckBox' or 'DCheckBoxLabel', menuThirdperson)
+    checkbox:Dock(TOP)
+    checkbox:DockMargin(0, 2, 0, 2)
 
-    local Checkbox = vgui.Create('DCheckBoxLabel', menu_thirdperson)
-    Checkbox:Dock(TOP)
-    Checkbox:DockMargin(0, 2, 0, 0)
-    Checkbox:SetText('Enable the third person')
-    Checkbox:SetConVar('third_person')
-
-    local slider_ud = vgui.Create('DNumSlider', menu_thirdperson)
+    local slider_ud = vgui.Create(Mantle and 'MantleSlideBox' or 'DNumSlider', menuThirdperson)
     slider_ud:Dock(TOP)
     slider_ud:DockMargin(2, 0, 0, 0)
     slider_ud:SetText('Up/down position')
-    slider_ud:SetMin(-15)
-    slider_ud:SetMax(15)
-    slider_ud:SetDecimals(0)
-    slider_ud:SetConVar('third_person_ud')
 
-    local slider_rl = vgui.Create('DNumSlider', menu_thirdperson)
+    local slider_rl = vgui.Create(Mantle and 'MantleSlideBox' or 'DNumSlider', menuThirdperson)
     slider_rl:Dock(TOP)
     slider_rl:DockMargin(2, 0, 0, 0)
     slider_rl:SetText('Right/left position')
-    slider_rl:SetMin(-20)
-    slider_rl:SetMax(20)
-    slider_rl:SetDecimals(0)
-    slider_rl:SetConVar('third_person_rl')
 
-    local slider_fb = vgui.Create('DNumSlider', menu_thirdperson)
+    local slider_fb = vgui.Create(Mantle and 'MantleSlideBox' or 'DNumSlider', menuThirdperson)
     slider_fb:Dock(TOP)
     slider_fb:DockMargin(2, 0, 0, 0)
     slider_fb:SetText('Forward/back position')
-    slider_fb:SetMin(-120)
-    slider_fb:SetMax(-30)
-    slider_fb:SetDecimals(0)
-    slider_fb:SetConVar('third_person_fb')
 
-    local slider_ang = vgui.Create('DNumSlider', menu_thirdperson)
+    local slider_ang = vgui.Create(Mantle and 'MantleSlideBox' or 'DNumSlider', menuThirdperson)
     slider_ang:Dock(TOP)
     slider_ang:DockMargin(2, 0, 0, 0)
     slider_ang:SetText('Camera rotation')
-    slider_ang:SetMin(-45)
-    slider_ang:SetMax(45)
-    slider_ang:SetDecimals(0)
-    slider_ang:SetConVar('third_person_ang')
+
+    if Mantle then
+        checkbox:SetTxt('Enable the third person')
+        checkbox:SetConvar('third_person')
+
+        slider_ud:SetRange(-15, 15, 0)
+        slider_ud:SetConvar('third_person_ud')
+
+        slider_rl:SetRange(-20, 20, 0)
+        slider_rl:SetConvar('third_person_rl')
+
+        slider_fb:SetRange(-120, -30, 0)
+        slider_fb:SetConvar('third_person_fb')
+
+        slider_ang:SetRange(-45, 45, 0)
+        slider_ang:SetConvar('third_person_ang')
+    else
+        menuThirdperson:ShowCloseButton(false)
+        menuThirdperson.Paint = function(_, w, h)
+            draw.RoundedBox(8, 0, 0, w, h, color_background)
+            draw.RoundedBoxEx(8, 0, 0, w, 24, color_header, true, true, false, false)
+        end
+        
+        local cls = vgui.Create('DButton', menuThirdperson)
+        cls:SetSize(16, 16)
+        cls:SetPos(menuThirdperson:GetWide() - cls:GetWide() - 4, 4)
+        cls:SetText('')
+        cls.DoClick = function()
+            menuThirdperson:Close()
+        end
+        cls.Paint = function(_, w, h)
+            draw.RoundedBox(6, 0, 0, w, h, color_close)
+        end
+
+        checkbox:SetText('Enable the third person')
+        checkbox:SetConVar('third_person')
+
+        slider_ud:SetMin(-15)
+        slider_ud:SetMax(15)
+        slider_ud:SetDecimals(0)
+        slider_ud:SetConVar('third_person_ud')
+
+        slider_rl:SetMin(-20)
+        slider_rl:SetMax(20)
+        slider_rl:SetDecimals(0)
+        slider_rl:SetConVar('third_person_rl')
+
+        slider_fb:SetMin(-120)
+        slider_fb:SetMax(-30)
+        slider_fb:SetDecimals(0)
+        slider_fb:SetConVar('third_person_fb')
+
+        slider_ang:SetMin(-45)
+        slider_ang:SetMax(45)
+        slider_ang:SetDecimals(0)
+        slider_ang:SetConVar('third_person_ang')
+    end
 end)
 
 // For Sandbox
